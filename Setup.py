39fa9c9f994031f3a86c40dd7f5c9d67f7e92359
Aditya Rai <adityarai0133@gmail.com>
2026-03-25 09:46:31 +0530

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rai-ram-optimizer-aadi", # Agar error aaye toh yahan naam thoda badal dena
+    version="1.0.0",
+    author="Aditya Rai",
+    author_email="adityarai0133@gmail.com", 
+    description="A lightweight RAM optimizer for legacy systems",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tera-username/rai-ram", # Apni GitHub repo ka link
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
